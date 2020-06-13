451e325641c48f7f40a802912fcad642d413cb31
HarunBuyuktepe <38882108+HarunBuyuktepe@users.noreply.github.com>
2020-06-13 16:04:19 +0300

    fifth commit

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -1,54 +1,48 @@
 <template>
   <v-container fluid fill-height>
-    <img src="../../assets/White-Tiger-Face-HD-1920x1080.jpg"
-         style="position: fixed; "/>
+    <img src="../../assets/White-Tiger-Face-HD-1920x1080.jpg" style="position: fixed; " />
     <v-row align="center" justify="center">
       <v-col>
-        <v-card
-          class="mx-auto"
-          max-width="400"
-          style="opacity: 0.92"
-        >
-          <v-card-title>
-            Settings
-          </v-card-title>
+        <v-card class="mx-auto" max-width="400" style="opacity: 0.92">
+          <v-card-title>Settings</v-card-title>
           <v-row class="ma-2" justify="center">
             <v-col cols="9" class="pa-2">
-              <v-text-field v-model="size1"
-                            label="Move Per Frame"
-                            append-icon="info"
-                            @click:append="sizeOne()"/>
+              <v-text-field
+                v-model="size1"
+                label="Move Per Frame"
+                append-icon="info"
+                @click:append="sizeOne()"
+              />
             </v-col>
           </v-row>
           <v-row class="ma-2" justify="center">
             <v-col cols="9" class="pa-2">
-              <v-text-field v-model="size2"
-                            label="Frame Number For Train"
-                            append-icon="info"
-                            @click:append="sizeTwo()"/>
+              <v-text-field
+                v-model="size2"
+                label="Frame Number For Train"
+                append-icon="info"
+                @click:append="sizeTwo()"
+              />
             </v-col>
           </v-row>
           <v-row class="ma-2" justify="center">
             <v-col cols="9" class="pa-2">
-              <v-text-field v-model="size3"
-                            label="Scroll Up/Down Threshold"
-                            append-icon="info"
-                            @click:append="sizeThree()"/>
+              <v-text-field
+                v-model="size3"
+                label="Scroll Up/Down Threshold"
+                append-icon="info"
+                @click:append="sizeThree()"
+              />
             </v-col>
           </v-row>
           <v-card-actions>
             <v-row justify="center">
-              <v-btn text dark color="pink" @click="goHome()">
-                Retun Home Page
-              </v-btn>
+              <v-btn text dark color="pink" @click="goHome()">Retun Home Page</v-btn>
             </v-row>
             <v-row justify="center">
-              <v-btn text dark color="pink">
-                Save And Calibrate
-              </v-btn>
+              <v-btn text dark color="pink" @click="saveSettings()">Save And Calibrate</v-btn>
             </v-row>
           </v-card-actions>
-
         </v-card>
       </v-col>
     </v-row>
@@ -59,7 +53,7 @@
         <v-card-text>{{ content }}</v-card-text>
 
         <v-card-actions>
-          <v-spacer/>
+          <v-spacer />
           <v-btn text @click="dialog = false">Ok</v-btn>
         </v-card-actions>
       </v-card>
@@ -67,49 +61,81 @@
   </v-container>
 </template>
 
-
 <script>
-
-  export default {
-    name: "Settings",
-    created() {
-      console.log("Setting Page")
-      this.user =this.$store.state.user
-      console.log(this.$store.state.user)
-      console.log(this.$route.params.username)
+export default {
+  name: "Settings",
+  created() {
+    console.log("Setting Page");
+    this.user = this.$store.state.user;
+    console.log(this.$store.state.user);
+    console.log(this.$route.params.username);
+    this.$http
+      .get("http://localhost:9099/get_user_settings/".concat(this.user))
+      .then(response => {
+        console.log(response.data);
+        this.size1 = response.data.MovePerFrame;
+        this.size2 = response.data.FrameNumberForTrain;
+        this.size3 = response.data.ScrollUpDownThreshold;
+      });
+  },
+  data() {
+    return {
+      currentTitle: "",
+      user: "",
+      content: "",
+      dialog: false,
+      size1: "",
+      size2: "",
+      size3: ""
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "Home", params: { username: this.user } });
     },
-    data() {
-      return {
-        currentTitle: "",
-        user: "",
-        content: "",
-        dialog: false,
-        size1: "",
-        size2: "",
-        size3: ""
-      }
+    sizeOne() {
+      this.dialog = true;
+      this.currentTitle = "Move Per Frame";
+      this.content = "";
     },
-    methods: {
-      goHome() {
-this.$router.push({name: "Home", params: {username: this.user}})
-      },
-      sizeOne() {
-        this.dialog = true;
-        this.currentTitle = "Move Per Frame"
-        this.content = ""
-      },
-      sizeTwo() {
-        this.dialog = true;
-        this.currentTitle = "Frame Number For Train"
-        this.content = "denem"
-      },
-      sizeThree() {
-        this.dialog = true;
-        this.currentTitle = "Scroll Up/Down Threshold"
-        this.content = "denem"
-      },
+    sizeTwo() {
+      this.dialog = true;
+      this.currentTitle = "Frame Number For Train";
+      this.content = "denem";
+    },
+    sizeThree() {
+      this.dialog = true;
+      this.currentTitle = "Scroll Up/Down Threshold";
+      this.content = "denem";
+    },
+    saveSettings() {
+      this.$http
+        .post("http://localhost:9099/set_user_settings/".concat(this.user), {
+          MovePerFrame: this.size1,
+          FrameNumberForTrain: this.size2,
+          ScrollUpDownThreshold: this.size3
+        })
+        .then(response => {
+          console.log(response.data);
+          this.size1 = response.data.MovePerFrame;
+          this.size2 = response.data.FrameNumberForTrain;
+          this.size3 = response.data.ScrollUpDownThreshold;
+        });
 
+      // this.$http
+      // 	.get("http://localhost:9099/calibrate/".concat(this.user))
+      // 	.then((response) => {
+      // 		console.log(response.data);
+      // 		if (response.data.message === "success") {
+      // 			this.$store.state.user = this.UserName;
+      // 			this.$router.push({
+      // 				name: "Home",
+      // 				params: { username: this.UserName },
+      // 			});
+      // 		}
+      // 		this.dialog = false;
+      // 	});
     }
   }
+};
 </script>
-
